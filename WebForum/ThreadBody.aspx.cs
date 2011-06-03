cc7ae2ae097fea38f06d1cd370be4db2547feab4
gafniv <gafniv@838de5ad-0dda-fd44-1b2e-fbc0efdb9928>
2011-06-03 15:44:07 +0000

    added support

--- WebForum/ThreadBody.aspx.cs
+++ WebForum/ThreadBody.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
+using ForumSever;
 
 namespace WebForum
 {
@@ -12,10 +13,48 @@ namespace WebForum
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            /*
             HtmlGenericControl acc = new HtmlGenericControl("div");
             acc.Attributes["name"] = "accordion1";
             addAccordions(acc);
             Panel1.Controls.Add(acc);
+             */
+            UserData ud = General.lm.getUserDataFromIP(Request.UserHostAddress);
+            PostsTree posts = General.lm.getThreadPostsAndContent(ud.CurForum._pIndex, ud.CurThread._pIndex, ud.Username);
+            string threadContent = General.lm.getThread(ud.CurForum._pIndex, ud.CurThread._pIndex).getContent();
+            HtmlGenericControl mainThread = setAccordions(posts, threadContent);
+            HtmlGenericControl wrapper = new HtmlGenericControl("div");
+            wrapper.Attributes["name"] = "accordion";
+            wrapper.Controls.Add(addHeader(ud.CurThread._subject));
+            wrapper.Controls.Add(mainThread);
+            Panel1.Controls.Add(wrapper);
+        }
+
+
+        private HtmlGenericControl setAccordions(PostsTree p_posts,string p_content)
+        {
+            HtmlGenericControl acc = new HtmlGenericControl("div");
+            acc.Attributes["name"] = "accordion";
+            acc.Controls.Add(addContent(p_content));
+            if (p_posts.Children != null)
+            {
+                foreach (PostsTree pt in p_posts.Children)
+                {
+                    acc.Controls.Add(addHeader(pt.Post._subject));
+                    acc.Controls.Add(setAccordions(pt, pt.Content));
+                }
+            }
+            return acc;
+        }
+
+        private HtmlGenericControl addContent(string p_content)
+        {
+            HtmlGenericControl div = new HtmlGenericControl("div");
+            HtmlGenericControl p = new HtmlGenericControl("p");
+            p.InnerText = p_content;
+            div.Controls.Add(p);
+            this.Controls.Add(div);
+            return div;
         }
 
         private void addAccordions(HtmlGenericControl acc)
